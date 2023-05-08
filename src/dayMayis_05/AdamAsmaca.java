795440de9c2c9d3401ac115ed42e55a91eafaf00
AdemOkurDE <ademokur.tr@gmail.com>
2023-05-08 08:51:49 +0200

    2023Mayis07

--- src/dayMayis_05/AdamAsmaca.java
+++ src/dayMayis_05/AdamAsmaca.java
@@ -1,5 +1,6 @@
 package dayMayis_05;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class AdamAsmaca {
@@ -13,24 +14,53 @@ public class AdamAsmaca {
 
         Bu programda, kelimeler adlı bir kelime listesi oluşturulacak.
         Ardından, bu listeden rastgele bir kelime seçmek için Math.random() fonksiyonu kullanılmıştır.
-        Kullanıcı, tahmin etmek istediği harfleri girebilir. Tahmin edilen harfler,
-        kelimenin doğru harfleriyle yer değiştirilir ve kalan hak sayısı tutulur.
+        Kullanıcı, tahmin etmek istediği harfleri girebilir.
+        Tahmin edilen harfler, kelimenin doğru harfleriyle yer değiştirilir ve kalan hak sayısı tutulur.
         Tahmin edilen kelimenin her aşaması ekrana yazdırılır.
         Kullanıcının tahmin hakkı bittiğinde, doğru kelime ekrana yazdırılır
  */
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        String[] kelimeler={"java","programlama","fonksiyon","yazilan","degisken","kodlama"};
-        String secilenKelime=kelimeler[(int) Math.random()* kelimeler.length]; // kelimeler array den rastgele bir kelime getir
+        String[] kelimeler={"java","programlama","fonksiyon","yazilan","degisken","kodlama","adana","kayseri","zonguldak","elma","armut","havuc","mandalina"};
+        int ratgeleSayi=(int)(Math.random()* kelimeler.length);
+
+        String secilenKelime=kelimeler[ratgeleSayi ]; // kelimeler array den rastgele bir kelime getir
+
+        //System.out.println( ratgeleSayi);
+        //System.out.println(secilenKelime);
         int kelimelength=secilenKelime.length();
-        System.out.println("lutfen bir harf giriniz:");
+        //System.out.println(kelimelength);
+
         int can=5;
+        System.out.println("kalan hak : "+can);
         String[] tahminEdilenHarfler=new String[secilenKelime.length()];
+
         for (int i = 0; i < tahminEdilenHarfler.length; i++) {
             tahminEdilenHarfler[i]="_";
         }
-        while (can<0){
+        System.out.println(Arrays.toString(tahminEdilenHarfler));
+        String harf="";
+        int bulunanHarf=0;
+        while (can>0&&bulunanHarf<secilenKelime.length()){
+            System.out.println("lutfen bir harf giriniz:");
+            harf= scanner.next().toLowerCase().substring(0,1);
+            if (secilenKelime.contains(harf)){
+                for (int i = 0; i < secilenKelime.length(); i++) {
+                    if (secilenKelime.substring(i,i+1).equals(harf)){
+                        tahminEdilenHarfler[i]=harf;
+                        bulunanHarf++;
+                    }
+                }
+                System.out.println(Arrays.toString(tahminEdilenHarfler));
+            }else can--;
 
+            System.out.println("kalan hak : "+can);
+        }
+        System.out.println(Arrays.toString(tahminEdilenHarfler));
+        if (can<=0){
+            System.out.println("Aradiginiz kelime : " + secilenKelime);
+        }else {
+            System.out.println("Tebrikler bildiniz!");
         }
 
     }
